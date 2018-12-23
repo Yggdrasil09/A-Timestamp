e35119336fa3292c5ca61debc0500b299d6e3f6f
Yggdrasil09 <karthik99th.tnk@gamil.com>
2018-12-23 11:19:22 +0530

    Added the routing to the time stamp api

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const http = require('http');
 const fs = require('fs');
 
+const getTimestamp = date => ({
+    unix : date.getTime(),
+    utc : date.toUTCString(),
+});
+
 const requestHandler = (req,res) => {
     if(req.url === "/")
     {
@@ -10,6 +15,26 @@ const requestHandler = (req,res) => {
             res.writeHead(200,{"Content-Type":"text/html"});
             res.end(html);
         });
+    }
+    else if (req.url.startsWith("/api/timestamp")){
+        const dateString = req.url.split("/api/timestamp/")[1];
+        let timestamp;
+        if (dateString === undefined || dateString.trim() === ""){
+            timestamp = getTimestamp(new Date());
+        } 
+        else {
+            const date = !isNaN(dateString)? new Date(parseInt(dateString)): new Date(dateString);
+            if (!isNaN(date.getTime())) {
+                timestamp = getTimestamp(date);
+            } 
+            else {
+                timestamp = {
+                error: "invalid date"
+                };
+            }
+        }
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(timestamp));
     }
 };
 
